fd4828e27c2bcdec3bc42bf11d48f3810d0e74d8
Jfigueroa79 <jordy.figueroam@ug.edu.ec>
2024-07-14 13:12:33 -0500

    Formularios Postulaciones Controllers y modelos

--- Backend/SSS/dao/postulacionesDao.py
+++ Backend/SSS/dao/postulacionesDao.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 class PostulacionesDao(BaseModel):
     s_opcion: int
@@ -8,7 +9,32 @@ class PostulacionesDao(BaseModel):
     s_plaza_laboral_id: Optional[int] = None
     s_cv: Optional[bytes] = None
     s_estado_solicitud_postulante_id: Optional[int] = None
+    s_fecha_entrevista: Optional[datetime] = None
+    s_formulario_entrevista: Optional[str] = None
+    s_telefono: Optional[str] = None
+    s_cargo: Optional[str] = None
+    s_banco: Optional[str] = None
+    s_sueldo: Optional[float] = None
+    s_cuenta_bancaria: Optional[str] = None
+    s_tipo_sangre: Optional[str] = None
 
 
 class CambiarEstadoPostulacion(BaseModel):
     s_postulacion_id: int
+
+class CambiarEstadoPostulacionEnProceso(BaseModel):
+    s_postulacion_id: int
+    s_fecha_entrevista: datetime
+
+class CambiarEstadoPostulacionEviarEntrevista(BaseModel):
+    s_postulacion_id: int
+    s_formulario_entrevista: str
+
+class CambiarEstadoPostulacionFinalizado(BaseModel):
+    s_postulacion_id: int
+    s_telefono: str
+    s_cargo: str
+    s_sueldo: float
+    s_cuenta_bancaria: str
+    s_tipo_sangre: str
+    s_banco:str
--- Backend/SSS/servicios/postulacionesService.py
+++ Backend/SSS/servicios/postulacionesService.py
@@ -3,7 +3,7 @@ from fastapi.security import HTTPAuthorizationCredentials
 from dao import conexion
 from dao.tokenizacionDao import Tokenizacion, auth_scheme, acciones
 from dao.ErrorResponse import extraer_mensaje_error
-from dao.postulacionesDao import PostulacionesDao,CambiarEstadoPostulacion
+from dao.postulacionesDao import PostulacionesDao,CambiarEstadoPostulacionEviarEntrevista,CambiarEstadoPostulacion,CambiarEstadoPostulacionEnProceso,CambiarEstadoPostulacionFinalizado
 import base64
 
 # Registrar nuevos endpoints en un nuevo APIRouter
@@ -33,8 +33,8 @@ async def postulaciones_consultar_estados_solicitud(bearer: HTTPAuthorizationCre
 
 @postulaciones.post("/postulacionesRegistrar")
 async def postulaciones_registrar(
-    s_usuario_id: int = Body(...),
-    s_plaza_laboral_id: int = Body(...),
+    s_usuario_id: str = Body(...),
+    s_plaza_laboral_id: str = Body(...),
     cv: UploadFile = File(...),
     bearer: HTTPAuthorizationCredentials = Depends(auth_scheme)
 ):
@@ -49,8 +49,8 @@ async def postulaciones_registrar(
         # Crear el DAO con los datos recibidos
         postulaciones_dao = PostulacionesDao(
             s_opcion=2,
-            s_usuario_id=s_usuario_id,
-            s_plaza_laboral_id=s_plaza_laboral_id,
+            s_usuario_id=int(s_usuario_id),
+            s_plaza_laboral_id=int(s_plaza_laboral_id),
             s_cv=cv_bytes
         )
         
@@ -64,7 +64,7 @@ async def postulaciones_registrar(
             else:
                 return {"resultado": "", "mensaje": False}
     except Exception as e:
-        return {"resultado": extraer_mensaje_error(str(e)), "mensaje": False}
+        return {"resultado": str(e), "mensaje": False}
     finally:
         conn.close()
 
@@ -124,6 +124,34 @@ async def postulaciones_consultar_por_usuario(s_usuario_id: int, bearer: HTTPAut
     finally:
         conn.close()
 
+@postulaciones.get("/postulacionesConsultarPorPlaza")
+async def postulaciones_consultar_por_plaza(s_plaza_laboral_id: int, bearer: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+    accion = acciones.get(Tokenizacion.ValidarToken(bearer), lambda: None)
+    if accion is not None:
+        return accion
+    conn = await conexion.conectar()
+    try:
+        postulaciones_dao = PostulacionesDao(s_opcion=10, s_plaza_laboral_id=s_plaza_laboral_id)
+        async with conn.cursor() as cur:
+            await cur.callproc('pr_postulaciones', tuple(dict(postulaciones_dao).values()))
+            result = await cur.fetchall()
+            await cur.nextset()
+            result2 = await cur.fetchall()
+            print(result,result2)
+             # Codificar el campo BLOB (cv) a base64
+            for row in result:
+                if 's_cv' in row and row['s_cv'] is not None:
+                    row['s_cv'] = base64.b64encode(row['s_cv']).decode('utf-8')
+            if result and result2:
+                return {"resultado": result, "mensaje": bool(result2[0]["mensaje"])}
+            else:
+                return {"resultado": "", "mensaje": True}
+    except Exception as e:
+        return {"resultado": extraer_mensaje_error(str(e)), "mensaje": False}
+    finally:
+        conn.close()
+        
+
 @postulaciones.put("/postulacionesModificar")
 async def postulaciones_modificar(
     s_usuario_id: int = Body(...),
@@ -162,13 +190,33 @@ async def postulaciones_modificar(
         conn.close()
 
 @postulaciones.put("/postulacionesCambiarEstadoEnProceso")
-async def postulaciones_cambiar_estado_en_proceso(objeto: CambiarEstadoPostulacion, bearer: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+async def postulaciones_cambiar_estado_en_proceso(objeto: CambiarEstadoPostulacionEnProceso, bearer: HTTPAuthorizationCredentials = Depends(auth_scheme)):
     accion = acciones.get(Tokenizacion.ValidarToken(bearer), lambda: None)
     if accion is not None:
         return accion
     conn = await conexion.conectar()
     try:
-        postulaciones_dao = PostulacionesDao(s_opcion=6, s_postulacion_id=objeto.s_postulacion_id)
+        postulaciones_dao = PostulacionesDao(s_opcion=6, s_postulacion_id=objeto.s_postulacion_id,s_fecha_entrevista=objeto.s_fecha_entrevista)
+        async with conn.cursor() as cur:
+            await cur.callproc('pr_postulaciones', tuple(dict(postulaciones_dao).values()))
+            result = await cur.fetchall()
+            if result:
+                return {"resultado": "", "mensaje": bool(result[0]["mensaje"])}
+            else:
+                return {"resultado": "", "mensaje": False}
+    except Exception as e:
+        return {"resultado": extraer_mensaje_error(str(e)), "mensaje": False}
+    finally:
+        conn.close()
+
+@postulaciones.post("/postulacionesEnviarEntrevista")
+async def postulaciones_enviar_entrevista(objeto: CambiarEstadoPostulacionEviarEntrevista, bearer: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+    accion = acciones.get(Tokenizacion.ValidarToken(bearer), lambda: None)
+    if accion is not None:
+        return accion
+    conn = await conexion.conectar()
+    try:
+        postulaciones_dao = PostulacionesDao(s_opcion=11, s_postulacion_id=objeto.s_postulacion_id,s_formulario_entrevista=objeto.s_formulario_entrevista)
         async with conn.cursor() as cur:
             await cur.callproc('pr_postulaciones', tuple(dict(postulaciones_dao).values()))
             result = await cur.fetchall()
@@ -202,20 +250,58 @@ async def postulaciones_cambiar_estado_informacion_personal(objeto: CambiarEstad
         conn.close()
 
 @postulaciones.put("/postulacionesCambiarEstadoFinalizado")
-async def postulaciones_cambiar_estado_finalizado(objeto: CambiarEstadoPostulacion, bearer: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+async def postulaciones_cambiar_estado_finalizado(objeto: CambiarEstadoPostulacionFinalizado, bearer: HTTPAuthorizationCredentials = Depends(auth_scheme)):
     accion = acciones.get(Tokenizacion.ValidarToken(bearer), lambda: None)
     if accion is not None:
         return accion
     conn = await conexion.conectar()
     try:
-        postulaciones_dao = PostulacionesDao(s_opcion=8, s_postulacion_id=objeto.s_postulacion_id)
+        postulaciones_dao = PostulacionesDao(
+            s_opcion=8, 
+            s_postulacion_id=objeto.s_postulacion_id,
+            s_telefono=objeto.s_telefono,
+            s_cargo=objeto.s_cargo,
+            s_banco=objeto.s_banco,
+            s_sueldo=objeto.s_sueldo,
+            s_cuenta_bancaria=objeto.s_cuenta_bancaria,
+            s_tipo_sangre=objeto.s_tipo_sangre
+        )
         async with conn.cursor() as cur:
             await cur.callproc('pr_postulaciones', tuple(dict(postulaciones_dao).values()))
             result = await cur.fetchall()
+            print(result)
             if result:
                 return {"resultado": "", "mensaje": bool(result[0]["mensaje"])}
             else:
                 return {"resultado": "", "mensaje": False}
+    except Exception as e:
+        return {"resultado": str(e), "mensaje": False}
+    finally:
+        conn.close()
+
+
+@postulaciones.get("/formularioDatosConsultarPorPostulacionId")
+async def formulario_datos_consultar_por_postulacion_id(s_postulacion_id: int, bearer: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+    accion = acciones.get(Tokenizacion.ValidarToken(bearer), lambda: None)
+    if accion is not None:
+        return accion
+    conn = await conexion.conectar()
+    try:
+        postulaciones_dao = PostulacionesDao(s_opcion=12, s_postulacion_id=s_postulacion_id)
+        async with conn.cursor() as cur:
+            await cur.callproc('pr_postulaciones', tuple(dict(postulaciones_dao).values()))
+            result = await cur.fetchall()
+            await cur.nextset()
+            result2 = await cur.fetchall()
+            print(result,result2)
+             # Codificar el campo BLOB (cv) a base64
+            for row in result:
+                if 's_cv' in row and row['s_cv'] is not None:
+                    row['s_cv'] = base64.b64encode(row['s_cv']).decode('utf-8')
+            if result and result2:
+                return {"resultado": result, "mensaje": bool(result2[0]["mensaje"])}
+            else:
+                return {"resultado": "", "mensaje": True}
     except Exception as e:
         return {"resultado": extraer_mensaje_error(str(e)), "mensaje": False}
     finally:
